fbe49fc612a5fd58590b912c6ace824e4a2a23e0
mole <hungrymolq@gmail.com>
2017-06-24 19:23:22 +0300

    Save debug files to separate dir

--- Helper.lua
+++ Helper.lua
@@ -1,14 +1,14 @@
 local inspect = require("inspect")
-function printToFile(t)
-    local filezz = io.open("log.txt", "a")
-    io.output(filezz)
+function printToFile(t, name, depth)
+    local file = io.open("debug/"..name..depth..".txt", "w")
+    io.output(file)
     io.write("\n")
     local options = {
-        depth = 2,
+        depth = depth,
 --        process = true
     }
     local result = inspect.inspect(t, options)
     io.write(result)
     io.write("\n")
-    io.close(filezz)
+    io.close(file)
 end
