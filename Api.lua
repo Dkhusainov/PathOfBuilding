cdfba366e49b9c1a3a515af2ffad771f47d95c7d
khusainov.d <hungrymolq@gmail.com>
2017-07-29 13:45:15 +0300

    Skill search filter results

--- Api.lua
+++ Api.lua
@@ -15,3 +15,44 @@ end
 function removeSkill(key)
     data[liveTargetVersion].skills[key] = nil
 end
+-----------------GemSearch--------------
+--GemSelectControl//function GemSelectClass:Draw(viewPort)
+function getSearchResult(self)
+    local result = {}
+    local t_insert = table.insert
+    for _, name in ipairs(self.list) do
+        local searchResultItem = {
+            name = name,
+            okSign = false,
+            plusSign = false,
+            signColor = colorCodes.NORMAL
+        }
+        local grantedEffect = self.skillsTab.build.data.gems[name]
+        if grantedEffect then
+            if grantedEffect.color == 1 then
+                searchResultItem.color = colorCodes.STRENGTH
+            elseif grantedEffect.color == 2 then
+                searchResultItem.color = colorCodes.DEXTERITY
+            elseif grantedEffect.color == 3 then
+                searchResultItem.color = colorCodes.INTELLIGENCE
+            end
+        end
+        if grantedEffect then
+            if grantedEffect.support and self.skillsTab.displayGroup.displaySkillList then
+                local gem = { grantedEffect = grantedEffect }
+                for _, activeSkill in ipairs(self.skillsTab.displayGroup.displaySkillList) do
+                    if calcLib.gemCanSupport(gem, activeSkill) then
+                        searchResultItem.okSign = true
+                        searchResultItem.signColor = self.sortCache.dpsColor[name]
+                        break
+                    end
+                end
+            elseif grantedEffect.hasGlobalEffect then
+                searchResultItem.plusSign = true
+                searchResultItem.signColor = self.sortCache.dpsColor[name]
+            end
+        end
+        t_insert(result, searchResultItem)
+    end
+    return result
+end
