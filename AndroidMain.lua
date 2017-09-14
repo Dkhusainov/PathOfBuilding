f8ddccdcd802269a28c7465cdda0a2480cd564ec
khusainov.d <hungrymolq@gmail.com>
2017-09-14 18:00:14 +0300

    Separate files

--- AndroidMain.lua
+++ AndroidMain.lua
@@ -5,174 +5,18 @@
 -- Time: 21:48
 -- To change this template use File | Settings | File Templates.
 --
+
 local treeText = ...
-inspect = require("inspect")
-json = require("json")
-serpent = require("serpent")
---unpack = require("unpack")
-function printTable(t, depth)
-    local file = io.open("debug.txt", "w")
-    io.output(file)
-    io.write("\n")
-    local options = {
-        depth = depth or 10,
-        --        process = true
-    }
-    local result = inspect.inspect(t, options)
-    print(result)
-    io.write(result)
-    io.write("\n")
-    io.close(file)
-end
+require("Enviroment")
 
-function tableToJson(t)
-    return json.encode(t)
-end
-
--- all the hacks for luaj to work
-bit = bit32
-function LoadModule(name, ...)
-    print("Loading " .. name .. "...")
-    local a = assert(loadfile(name..".lua"))
-    return a(...)
-end
-
-function ConPrintf(text, ...)
-    local t = string.format(text, ...)
-    print(t)
-end
-
-function MakeDir(a) end
-
-function unpack(t, i)
-    i = i or 1		--single argument (like below), i defaults to 1
-    if t[i] then
-        return t[i], unpack(t, i+1)		--recursion
+local ok, tree = serpent.load(treeText)
+main.tree[liveTargetVersion] = tree
+for _, node in ipairs(tree.nodes) do
+    local modList = common.New("ModList")
+    for k, v in ipairs(node.modList) do
+        modList[k] = v
     end
-end
-
-function GetTime() return 0 end
-function NewImageHandle() return 0 end
-
-defaultTargetVersion = "3_0"
-liveTargetVersion = "3_0"
-targetVersionList = { "3_0" }
-
-launch = {
-    devMode = false,
-    ShowErrMsg = function(title, msg) end
-}
-main = {
-    tree = {},
-    uniqueDB = { },
-    rareDB = { },
-    showThousandsSidebar = false,
-    lastShowThousandsSidebar = false,
-    --todo show dialogs
-    OpenConfirmPopup = function(main, title, msg, confirmLabel, onConfirm) onConfirm() end,
-    OpenMessagePopup = function(title, msg) end
-}
-
-LoadModule("Api")
-LoadModule("Modules/Common")
-LoadModule("Modules/Data", launch)
-LoadModule("Modules/ModTools", launch)
-LoadModule("Modules/ItemTools", launch)
-LoadModule("Modules/CalcTools", launch)
-
-local classList = {
-    "Control",
-    "Tooltip",
-    "TooltipHost",
-    "ControlHost",
-    "UndoHandler",
-    "ScrollBarControl",
-    "ButtonControl",
-    "ListControl",
-    "TextListControl",
-    "DropDownControl",
-    "EditControl",
-    "LabelControl",
-    "CheckBoxControl",
-    "CalcSectionControl",
-    "CalcBreakdownControl",
-    "SectionControl",
-    "GemSelectControl",
-    "PassiveTree",
-    "PassiveTreeView",
-    "PassiveSpec",
-    "PassiveSpecListControl",
-    "PassiveTreeView",
-    "SliderControl",
-    "ItemSlotControl",
-    "ItemListControl",
-    "SharedItemListControl",
-    "ItemDBControl",
-    "ModDB",
-    "ModList",
-    "SkillList",
-    "TreeTab",
-    "ConfigTab",
-    "SkillsTab",
-    "ItemsTab",
-    "CalcsTab"
-}
-
-for _, className in pairs(classList) do
-    LoadModule("Classes/"..className, launch, main)
-end
-
-for _, targetVersion in ipairs(targetVersionList) do
-    LoadModule("Data/"..targetVersion.."/ModCache", modLib.parseModCache[targetVersion])
-    if treeText ~= nil then
-        local ok, tree = serpent.load(treeText)
-        main.tree[targetVersion] = tree
-        for _, node in ipairs(tree.nodes) do
-            local modList = common.New("ModList")
-            for k, v in ipairs(node.modList) do
-                modList[k] = v
-            end
-            node.modList = modList
-        end
-    end
-end
-
-for _, targetVersion in ipairs(targetVersionList) do
-    main.uniqueDB[targetVersion] = { list = { } }
-    for type, typeList in pairs(data.uniques) do
-        for k, raw in pairs(typeList) do
-            local newItem = itemLib.makeItemFromRaw(targetVersion, "Rarity: Unique\n"..raw)
-            if newItem then
-                itemLib.normaliseQuality(newItem)
-                main.uniqueDB[targetVersion].list[newItem.name] = newItem
-            else
-                ConPrintf("Unique DB unrecognised item of type '%s':\n%s", type, raw)
-            end
-            typeList[k] = nil
-        end
-    end
-    data.uniques = nil
-    main.rareDB[targetVersion] = { list = { } }
---    for _, raw in pairs(data[targetVersion].rares) do
---        local newItem = itemLib.makeItemFromRaw(targetVersion, "Rarity: Rare\n"..raw)
---        if newItem then
---            itemLib.normaliseQuality(newItem)
---            if newItem.crafted then
---                if newItem.base.implicit and (#newItem.modLines == 0 or newItem.modLines[1].custom) then
---                    newItem.implicitLines = 0
---                    for line in newItem.base.implicit:gmatch("[^\n]+") do
---                        local modList, extra = modLib.parseMod[targetVersion](line)
---                        newItem.implicitLines = newItem.implicitLines + 1
---                        t_insert(newItem.modLines, newItem.implicitLines, { line = line, extra = extra, modList = modList or { } })
---                    end
---                end
---                itemLib.craftItem(newItem)
---            end
---            self.rareDB[targetVersion].list[newItem.name] = newItem
---        else
---            ConPrintf("Rare DB unrecognised item:\n%s", raw)
---        end
---    end
+    node.modList = modList
 end
 
 build = LoadModule("Modules/Build", launch, main)
@@ -183,4 +27,18 @@ function initBuild(name, xml)
 end
 function saveBuild()
     return build:SaveDB()
+end
+
+function loadUnique(raw)
+    local newItem = itemLib.makeItemFromRaw(liveTargetVersion, "Rarity: Unique\n"..raw)
+    if newItem then
+        itemLib.normaliseQuality(newItem)
+        main.uniqueDB[liveTargetVersion].list[newItem.name] = newItem
+    else
+        ConPrintf("Unique DB unrecognised item of type '%s':\n%s", type, raw)
+    end
+end
+
+function removeUnique(name)
+    main.uniqueDB[liveTargetVersion].list[name] = nil
 end
---
+++ Enviroment.lua
@@ -0,0 +1,125 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: mole
+-- Date: 21.06.2017
+-- Time: 21:48
+-- To change this template use File | Settings | File Templates.
+--
+inspect = require("inspect")
+json = require("json")
+serpent = require("serpent")
+function printTable(t, depth)
+    local file = io.open("debug.txt", "w")
+    io.output(file)
+    io.write("\n")
+    local options = {
+        depth = depth or 10,
+        --        process = true
+    }
+    local result = inspect.inspect(t, options)
+    print(result)
+    io.write(result)
+    io.write("\n")
+    io.close(file)
+end
+
+function tableToJson(t)
+    return json.encode(t)
+end
+
+-- all the hacks for luaj to work
+bit = bit32
+function MakeDir(a) end
+function GetTime() return 0 end
+function NewImageHandle() return 0 end
+function LoadModule(name, ...)
+    print("Loading " .. name .. "...")
+    local a = assert(loadfile(name..".lua"))
+    return a(...)
+end
+
+function ConPrintf(text, ...)
+    local t = string.format(text, ...)
+    print(t)
+end
+
+function unpack(t, i)
+    i = i or 1		--single argument (like below), i defaults to 1
+    if t[i] then
+        return t[i], unpack(t, i+1)		--recursion
+    end
+end
+
+--POB stuff
+defaultTargetVersion = "3_0"
+liveTargetVersion = "3_0"
+targetVersionList = { "3_0" }
+
+launch = {
+    devMode = false,
+    ShowErrMsg = function(title, msg) end
+}
+launch.full = treeText == nil
+
+main = {
+    tree = {},
+    uniqueDB = { },
+    rareDB = { },
+    showThousandsSidebar = false,
+    lastShowThousandsSidebar = false,
+    --todo show dialogs
+    OpenConfirmPopup = function(main, title, msg, confirmLabel, onConfirm) onConfirm() end,
+    OpenMessagePopup = function(title, msg) end
+}
+main.uniqueDB[liveTargetVersion] = { list = { } }
+main.rareDB[liveTargetVersion] = { list = { } }
+
+LoadModule("Api")
+LoadModule("Modules/Common")
+LoadModule("Modules/Data", launch)
+LoadModule("Modules/ModTools", launch)
+LoadModule("Modules/ItemTools", launch)
+LoadModule("Modules/CalcTools", launch)
+
+local classList = {
+    "Control",
+    "Tooltip",
+    "TooltipHost",
+    "ControlHost",
+    "UndoHandler",
+    "ScrollBarControl",
+    "ButtonControl",
+    "ListControl",
+    "TextListControl",
+    "DropDownControl",
+    "EditControl",
+    "LabelControl",
+    "CheckBoxControl",
+    "CalcSectionControl",
+    "CalcBreakdownControl",
+    "SectionControl",
+    "GemSelectControl",
+    "PassiveTree",
+    "PassiveTreeView",
+    "PassiveSpec",
+    "PassiveSpecListControl",
+    "PassiveTreeView",
+    "SliderControl",
+    "ItemSlotControl",
+    "ItemListControl",
+    "SharedItemListControl",
+    "ItemDBControl",
+    "ModDB",
+    "ModList",
+    "SkillList",
+    "TreeTab",
+    "ConfigTab",
+    "SkillsTab",
+    "ItemsTab",
+    "CalcsTab"
+}
+
+for _, className in pairs(classList) do
+    LoadModule("Classes/"..className, launch, main)
+end
+
